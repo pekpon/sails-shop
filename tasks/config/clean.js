9eed7f942f8c76809706d326a8c8e9ba9abf9d34
Eric Ponce <ponceeric@gmail.com>
2015-12-14 17:36:54 +0100

    clean tmp

--- tasks/config/clean.js
+++ tasks/config/clean.js
@@ -9,12 +9,32 @@
  * For usage docs see:
  * 		https://github.com/gruntjs/grunt-contrib-clean
  */
+//module.exports = function(grunt) {
+//
+//	grunt.config.set('clean', {
+//		dev: ['.tmp/public/**'],
+//		build: ['www']
+//	});
+//
+//	grunt.loadNpmTasks('grunt-contrib-clean');
+//};
+//-----------OLD-------------
+
 module.exports = function(grunt) {
+    grunt.config.set('clean', {
+      dev: [
+        getFolderPath('fonts/**'),
+        getFolderPath('images/**'),
+        getFolderPath('js/**'),
+        getFolderPath('styles/**'),
+        getFolderPath('*.*')
+      ],
+      build: ['www']
+    });
 
-	grunt.config.set('clean', {
-		dev: ['.tmp/public/**'],
-		build: ['www']
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 };
+
+function getFolderPath(folderName){
+  return '.tmp/public/' + folderName;
+}
